0af9c596d4c2c7311cefad2a8072263a9c117960
JudeEWalsh <walshjude63@gmail.com>
2026-02-12 16:06:08 +0000

    Add Models and Views

--- controllers/about.js
+++ controllers/about.js
@@ -1,11 +1,19 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import employee from "../models/employee.js"
 
 const about = {
     createView(request, response) {
         logger.info("About page loading!")
-        response.send('About the Playlist app');
+
+        const viewData = {
+            title: "About the Playlist app",
+            emp: employee.getAppInfo()
+        };
+
+        logger.info(viewData.emp)
+        response.render('about', viewData);
     }
 }
 
--- controllers/start.js
+++ controllers/start.js
@@ -1,11 +1,19 @@
 'use strict';
 
 import logger from "../utils/logger.js";
+import appStore from "../models/app-store.js";
 
 const start = {
+
     createView(request, response) {
-        logger.info("Start page loading!")
-        response.send("Welcome to Jude's Playlist app!");
+        logger.info("Start page loading!");
+
+        const viewData = {
+            title: "Welcome to the Playlist app!",
+            info: appStore.getAppInfo()
+        };
+
+        response.render('start', viewData);
     },
 };
 
