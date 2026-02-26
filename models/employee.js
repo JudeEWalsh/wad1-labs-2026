44e7032f35ec28d87223f4c9c39a8157b1c9fa17
JudeEWalsh <walshjude63@gmail.com>
2026-02-26 17:54:16 +0000

    Completed Dynamic Views and Helpers Lab

--- models/employee.js
+++ models/employee.js
@@ -5,8 +5,8 @@ import JsonStore from './json-store.js';
 
 const empStore = {
 
-    store: new JsonStore('./models/employee.json', { employee: {} }),
-    collection: 'employee',
+    store: new JsonStore('./models/employee.json', { employees: {} }),
+    collection: 'employees',
 
     getAppInfo() {
         return this.store.findAll(this.collection);
